131d072e487e2ed1d8ceb5aadb7efa420ddd5e57
Sabrina <sabrina.wong@cix.csi.cuny.edu>
2022-11-21 18:26:42 -0500

    commit tree 17b27868f950f3c2ab3923824056c113fbce2fc4

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue){
+    if (someValue >= 42){
+        return someValue - 42;
+    }
+   else{ 
+    return 42 - someValue; 
+
+   }
+
+}
+
+function distanceFromHqInFeet(someValue){
+   return distanceFromHqInBlocks (someValue) * 264;
+
+}
+
+function distanceTravelledInFeet(start,destination){
+    if (start >= destination){
+        return (start - destination) * 264;
+    }
+    else
+    return (destination - start) * 264;
+}
+
+function calculatesFarePrice(start, destination){
+    const feetTravelledInCab = (distanceTravelledInFeet(start,destination));
+
+    if (feetTravelledInCab > 2500){
+
+    return 'cannot travel that far'
+     
+}
+else if (feetTravelledInCab <= 400){
+    return 0;
+}
+else if (feetTravelledInCab > 400 && feetTravelledInCab <= 2000){
+    return (feetTravelledInCab - 400 ) * 0.02;
+
+}
+else if (feetTravelledInCab > 2000 && feetTravelledInCab < 2500)
+return 25;
+}
